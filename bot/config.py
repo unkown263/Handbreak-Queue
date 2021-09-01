80babd0720705a8330a0666e5b4addf7de333631
unkown263 <75568317+unkown263@users.noreply.github.com>
2021-09-01 20:23:59 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -21,10 +21,8 @@ try:
     BOT_TOKEN = config("BOT_TOKEN")
     DEV = 1322549723
     OWNER = config("OWNER")
-    FFMPEG = config(
-        "FFMPEG",
-        default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
-    )
+    HANDBREAK_CLI = config(
+        "HandBrakeCLI")
     THUMB = config(
         "THUMBNAIL", default="https://telegra.ph/file/75ee20ec8d8c8bba84f02.jpg"
     )
